722035d66acc37f758aea47eb6a33793c4356062
Pedro Guia <u202212010@upc.edu.pe>
2024-09-25 23:53:24 -0500

    feat(pages): added profile component.

---
+++ src/app/public/pages/profile/profile.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatInput} from "@angular/material/input";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [
+    MatToolbar,
+    MatInput,
+    MatIconButton,
+    MatIcon
+  ],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+  user = {
+    firstname: 'Benito Bruno',
+    lastname: 'Benitez Balbuena',
+    email: 'benitobenitez@gmail.com',
+    role: 'Alumno'
+  }
+}
