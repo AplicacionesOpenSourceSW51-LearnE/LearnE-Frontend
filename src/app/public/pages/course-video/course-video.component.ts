79d72d0b3a36809623510cc417d481f92a1e5efa
AnthonyHuapaya <u202220235@upc.edu.pe>
2024-09-26 05:11:22 -0500

    update courseVideo component tutorial

--- src/app/public/pages/course-video/course-video.component.ts
+++ src/app/public/pages/course-video/course-video.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
-import {MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {NgIf} from "@angular/common";
-import {ActivatedRoute} from "@angular/router";
-import {CourseService} from "../../../learning/services/course.service";
-import {MatHint, MatSuffix} from "@angular/material/form-field";
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
-import {MatInput} from "@angular/material/input";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { MatSidenavContainer, MatSidenavContent } from "@angular/material/sidenav";
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import {NgForOf, NgIf} from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
+import { CourseService } from "../../../learning/services/course.service";
+import { MatFormField, MatHint, MatLabel, MatSuffix } from "@angular/material/form-field";
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle
+} from "@angular/material/datepicker";
+import { MatInput } from "@angular/material/input";
+import { MatNativeDateModule } from "@angular/material/core";
+import {MatListItem} from "@angular/material/list";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-course-video',
@@ -19,29 +27,37 @@ import {MatInput} from "@angular/material/input";
     NgIf,
     MatSidenavContainer,
     MatHint,
+    MatLabel,
     MatDatepickerToggle,
+    MatDatepickerModule,
     MatDatepicker,
     MatSuffix,
     MatDatepickerInput,
-    MatInput
+    MatInput,
+    MatFormField,
+    MatNativeDateModule,
+    NgForOf,
+    MatListItem,
+    MatButton
   ],
   templateUrl: './course-video.component.html',
-  styleUrl: './course-video.component.css'
+  styleUrls: ['./course-video.component.css'] // Debe ser styleUrls, no styleUrl
 })
 export class CourseVideoComponent {
   sectionId: number | null = null;
   videoUrl: SafeResourceUrl = '';
   isLocalVideo: boolean = false;
+  section: any;
 
-  constructor(private sanitizer: DomSanitizer, private route: ActivatedRoute, private courseService: CourseService) {}
+  constructor(private sanitizer: DomSanitizer, private route: ActivatedRoute, private courseService: CourseService) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       console.log('queryParams: ', params);
       const id = params['id'];
-      this.sectionId = id ? Number(id): null;
+      this.sectionId = id ? Number(id) : null;
       console.log('sectionId: ', this.sectionId);
-    })
+    });
     if (this.sectionId) {
       this.loadVideo();
     }
@@ -52,14 +68,12 @@ export class CourseVideoComponent {
   }
 
   loadVideo() {
-    const section = this.courseService.getSectionById(this.sectionId || 0);
-    if (section && section.link) {
-      this.videoUrl = this.sanitizeUrl(section.link);
-      this.isLocalVideo = section.link.endsWith('.mp4');
+    this.section = this.courseService.getSectionById(this.sectionId || 0);
+    if (this.section && this.section.link) {
+      this.videoUrl = this.sanitizeUrl(this.section.link);
+      this.isLocalVideo = this.section.link.endsWith('.mp4');
     } else {
       console.error('No se encontró la sección o el enlace es inválido');
     }
   }
-
-
 }
