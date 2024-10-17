882c9ed44fa0a4aa79905f4697dccaac829007bd
Pedro Guia <u202212010@upc.edu.pe>
2024-10-17 00:21:20 -0500

    feat(pages): added router links profile, my notes and course history.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,19 @@ import {RegisterComponent} from "./public/pages/register/register.component";
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {SuscriptionsComponent} from "./public/pages/subscriptions/suscriptions.component";
 import {PaymentComponent} from "./public/pages/payment/payment.component";
+import {ProfileComponent} from "./public/pages/profile/profile.component";
+import {MyNotesComponent} from "./public/pages/my-notes/my-notes.component";
+import {CourseHistoryComponent} from "./public/pages/course-history/course-history.component";
 
 export const routes: Routes = [
   { path: 'home',           component: LandingPageComponent},
   { path: 'signIn',         component: SignInComponent},
   { path: 'register',       component: RegisterComponent},
   { path: 'subscriptions',  component: SuscriptionsComponent},
+  { path: 'courseHistory',  component: CourseHistoryComponent},
   { path: 'payment',        component: PaymentComponent},
+  { path: 'myNotes',        component: MyNotesComponent},
+  { path: 'profile',        component: ProfileComponent},
   { path: '',               redirectTo: 'home', pathMatch: 'full' },
   { path: '**',             component: PageNotFoundComponent }
 ];
