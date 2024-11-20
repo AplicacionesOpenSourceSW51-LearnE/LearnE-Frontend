344263e89cde0890b3ece670ffb3c104e23d670c
AnthonyHuapaya <anthonyhuacue@gmail.com>
2024-11-20 17:38:22 -0500

    development 3.0 correct sign in

--- src/app/iam/pages/sign-in/sign-in.component.ts
+++ src/app/iam/pages/sign-in/sign-in.component.ts
@@ -1,14 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatButtonModule} from "@angular/material/button";
 import {FormsModule} from "@angular/forms";
 import {MatInput} from "@angular/material/input";
 import {MatLabel} from "@angular/material/form-field";
-import {RouterModule} from "@angular/router";
-import {AuthenticationService} from "../../services/authentication.service";
-import {BaseFormComponent} from "../../../shared/components/base-form.component";
-import {SignInRequest} from "../../model/sign-in.request";
+import {Router, RouterLink, RouterModule} from "@angular/router";
+import {UserService} from "../../../learning/services/user.service";
+import {User} from "../../../learning/model/user.entity";
+import {Course} from "../../../learning/model/course.entity";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-sign-in',
@@ -21,26 +22,58 @@ import {SignInRequest} from "../../model/sign-in.request";
     MatInput,
     MatLabel,
     RouterModule,
+    RouterLink,
+    TranslateModule
   ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
-export class SignInComponent extends BaseFormComponent{
-  username: string;
+export class SignInComponent {
+  users: Array<User> = [];
+  email: string;
   password: string;
-  submitted = false;
 
-  constructor(private authenticationService: AuthenticationService) {
-    super();
-    this.username = "";
+  constructor(private userApiService: UserService, private router: Router) {
+    this.email = "";
     this.password = "";
+
   }
-  validateUser() {
-    if (this.username == "" || this.password == "") return;
-    let username = this.username;
-    let password = this.password;
-    const signInRequest = new SignInRequest(username, password);
-    this.authenticationService.signIn(signInRequest)
-    this.submitted = true;
+
+  ngOnInit(){
+    this.getAllUsers();
+  }
+
+  private getAllUsers() {
+    this.userApiService.getAll().subscribe((response: Array<User>) => {
+      this.users = response;
+      console.log(this.users);
+    })
+  }
+
+  private enterUser() {
+    if (Number(sessionStorage.getItem('type_user')) == 1) {
+      this.router.navigate(['/mainPage/catalog']);
+    } else {
+      this.router.navigate(['/mainPage/management']);
+    }
+  }
+
+  public validateUser() {
+    let user1 = new User({});
+    for (let user of this.users) {
+      user1 = user;
+      if (user1.email == this.email) {
+        if (user1.password == this.password) {
+          sessionStorage.setItem('email', this.email);
+          sessionStorage.setItem('password', this.password);
+          sessionStorage.setItem('type_user', String(user1.type_user));
+          sessionStorage.setItem('id', String(user1.id));
+          sessionStorage.setItem('type_plan', String(user1.type_plan));
+          this.enterUser();
+        } else {
+          alert('Password or email incorrect');
+        }
+      }
+    }
   }
 }
